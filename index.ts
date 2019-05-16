872d1fc01d6501bddfcceda219fb4b1e847efcb2
Aman Gupta <agupta1@berkadia.com>
2019-05-17 02:33:07 +0530

    Initial release of search_customers npm module

---
+++ index.ts
@@ -0,0 +1,40 @@
+/**
+* @Method: Returns the list of customers for which the distance is <= 100kms.
+* @Param {customer_records[](json),latitude(Dublin latitude in radians),longitude}
+* @Return {customers_list}
+*/
+export function find_customers(customer_records: any, latitude: number, longitude: number): any {
+    let customers_list = [];
+    const radius_earth = 6371; // radius of earth
+    for (var i = 0; i < customer_records.length; i++) {
+        var lat1 = degrees_to_radians(latitude);
+        var long1 = degrees_to_radians(longitude);
+        var lat2 = degrees_to_radians(customer_records[i].latitude);
+        var long2 = degrees_to_radians(customer_records[i].longitude);
+        var distanceBetweenTwoPoints = getDistance(lat1, lat2, long1, long2, radius_earth);
+        if (distanceBetweenTwoPoints <= 100) {
+            customers_list.push({ 'name': customer_records[i].name, 'user_id': customer_records[i].user_id })
+        }
+    }
+    return customers_list;
+}
+
+/**
+* @Method: Convert the Degrees to radian by taking degrees as input
+* @Param {degrees:number}
+* @Return {radian:number}
+*/
+export function degrees_to_radians(degrees: any) {
+    const pi = Math.PI;
+    return degrees * (pi / 180);
+}
+
+/**
+* @Method: Returns the distance between two points from Dublin Area to customer point .
+* @Param {lat1:number(Dublin Latitude),lat2:(Customer Latitude),long1(Dublin Longitude),long2(Customer Longitude)}
+* @Return {distance:number}
+*/
+export function getDistance(lat1: number,lat2: number,long1: number,long2: number,radius: number) {
+    const angle = Math.acos(Math.sin(lat1)*Math.sin(lat2) + Math.cos(lat1)*Math.cos(lat2)*Math.cos(Math.abs(long1-long2)));
+    return radius*angle;
+}
---
+++ test/test.js
@@ -0,0 +1,55 @@
+'use strict';
+var expect = require('chai').expect;
+var index = require('../dist/index.js');
+describe('find_customers function test', () => {
+
+    it('should return 1 record in customers_list whose distance is less than or equal to 100km', () => {
+        const json = [
+            { "latitude": "52.986375", "user_id": 12, "name": "Christina McArdle", "longitude": "-6.043701" },
+            { "latitude": "51.92893", "user_id": 1, "name": "Alice Cahill", "longitude": "-10.27699" },
+            { "latitude": "51.8856167", "user_id": 2, "name": "Ian McArdle", "longitude": "-10.4240951" },
+        ];
+        const dublin_latitude = 53.339428;
+        const dublin_longitude = -6.257664;
+        const result = index.find_customers(json, dublin_latitude, dublin_longitude);
+        expect(result).to.have.lengthOf(1);
+    });
+
+    it('should return 0 record in customers_list whose distance is less than or equal to 100km', () => {
+        const json = [
+            { "latitude": "51.92893", "user_id": 1, "name": "Alice Cahill", "longitude": "-10.27699" },
+            { "latitude": "51.8856167", "user_id": 2, "name": "Ian McArdle", "longitude": "-10.4240951" },
+        ];
+        const dublin_latitude = 53.339428;
+        const dublin_longitude = -6.257664;
+        const result = index.find_customers(json, dublin_latitude, dublin_longitude);
+        expect(result).to.have.lengthOf(0);
+    });
+});
+
+describe('degrees_to_radians function test', () => {
+
+    it('convert degrees_to_radians for dublin latitude', () => {
+        const dublin_latitude = 53.339428;
+        const result = index.degrees_to_radians(dublin_latitude);
+        expect(result).to.equal(0.9309486397304539);
+    });
+
+    it('convert degrees_to_radians for dublin longitude', () => {
+        const dublin_longitude = -6.257664;
+        const result = index.degrees_to_radians(dublin_longitude);
+        expect(result).to.equal(-0.10921684028351844);
+    });
+});
+
+describe('getDistance function test', () => {
+    it('should getDistance between dublin area and one of customer area', () => {
+        const dublin_latitude = 53.339428;
+        const customer_latitude = 52.339428;
+        const dublin_longitude = -6.257664;
+        const customer_longitude = -5.257664;
+        const radius_earth = 6371;
+        const result = index.getDistance(dublin_latitude,customer_latitude,dublin_longitude,customer_longitude,radius_earth);
+        expect(result).to.equal(7938.041920231655);
+    });
+});
